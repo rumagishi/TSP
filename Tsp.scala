1a081b19373e8b2b8c15d545c459ba11448571e4
rumagishi <rumagishi@gmail.com>
2015-07-28 10:16:02 +0900

    somewhat parallelized

--- Tsp.scala
+++ Tsp.scala
@@ -2,11 +2,13 @@ import scala.io.Source
 import math._
 import scala.util.Random
 import scala.util.Random._
+import scala.actors.Actor
+import scala.actors.Actor._
 
 object Tsp {
 
   /*citycordiから各都市間の距離を算出するメソッド*/
-  def getOverallDistance(cor: Array[(Int, Int)]) = {
+  def getOverallDistance(cor: Array[(Int, Int)]): Array[Array[Double]]= {
     val len = cor.length
     var distance = Array.ofDim[Double](len, len)
     for(i <- 0 until len; j <- 0 until len) {
@@ -15,8 +17,13 @@ object Tsp {
     distance
   }
 
+  /*都市の巡る順番をバラバラにするメソッド(初期探索点)*/
+  def shuffleRoute(order: List[Int]): List[Int] = {
+    shuffle(order)
+  }
+
   /*経路の道のりを求めるメソッド*/
-  def getRouteDistance(order: List[Int], distanceTable: Array[Array[Double]]) = {
+  def getRouteDistance(order: List[Int], distanceTable: Array[Array[Double]]): Double = {
     val len = order.length
     var dis = 0.0
     for (i <- 0 until len-1) {
@@ -26,51 +33,92 @@ object Tsp {
     dis
   }
 
-  /*都市の巡る順番をバラバラにするメソッド(初期探索点)*/
-  def shuffleRoute(order: List[Int], seed: Int) = {
-    Random.setSeed(seed)
-    shuffle(order)
-  }
-
-  /*2-optして近傍で最良の解を求めるメソッド*/
-  def twoOpt(order: List[Int], distanceTable: Array[Array[Double]]) = {
-    val len = order.length
-    val candidates = for(i <- 0 until len-1; j <- i+1 until len) yield order.updated(i,order(j)).updated(j,order(i))
+  /*最良の経路を返す*/
+  def returnBestRoute(candidates: List[List[Int]], distanceTable: Array[Array[Double]]): List[Int] = {
     val dis = candidates.map(x => getRouteDistance(x, distanceTable))
     val bestNeighborhood = candidates(dis.indexOf(dis.min))
-    if (dis.min < getRouteDistance(order, distanceTable)) {
-      bestNeighborhood
-    } else {
-      order
-    }
+    bestNeighborhood
+  }
+
+/*2-optして近傍で最良の解を求めるメソッド*/
+  def twoOpt(order: List[Int]): List[List[Int]] = {
+    val len = order.length
+    var candidates = for(i <- 0 until len-1; j <- i+1 until len) yield order.updated(i,order(j)).updated(j,order(i)).toList
+    order :: candidates.toList
   }
 
   /*内包できるメソッドはしたほうがいいかも*/
 
   def main(args: Array[String]): Unit = {
+    /*問題の初期設定*/
     /*ここはdatファイルから読み込めるようにする*/
    val citycordi:Array[(Int, Int)] = Array( (0,0), (1,0), (2,0), (2,1), (2,2), (1,2), (0,2), (0,1) )
-   val defaultRoute = (0 until citycordi.length).toList
-   val seed = args(0).toInt
-   //val numberOfCity = citycordi.length
+   /*都市間の距離をあらかじめ求めておく*/
    val distanceTable = getOverallDistance(citycordi)
-    /*2-optする．タプルの最初から都市名を特定*/
-    /*2-optしたものの距離をもとめる*/
-   val routeDistance = getRouteDistance(defaultRoute, distanceTable)
-    /*訪問順番のシャッフル*/
-   val newOrder = shuffleRoute(defaultRoute, seed)
-   println(newOrder)
-   println(defaultRoute)
-   val checkTwoOpt = twoOpt(defaultRoute, distanceTable)
-   println(checkTwoOpt)
+   /*都市の巡回順序*/
+   val defaultRoute = (0 until citycordi.length).toList
+   /*シード値を読み込む*/
+   val seed = args(0).toInt
+   Random.setSeed(seed)
+   val genNum = 4 //探索開始地点数．これも実行時に指定できるようにする．
+
+   /*ルートをgenNumの数だけシャッフル*/
+   var routes = for(i <- 0 until genNum) yield shuffleRoute(defaultRoute)
+   routes :+= defaultRoute
+   println(routes)
+   val findBest = actor {
+     loop {
+       react {
+         case candidates: List[List[Int]] => {
+           reply(returnBestRoute(candidates, distanceTable)) //Vector(List[Int]) length = 4(routes.length)
+         }
+       }
+     }
+   }
+   val samplepoint = actor {
+     var tempList = List[Int]()
+     def func(route: List[Int]): Unit = {
+       tempList = route
+       val candidates = twoOpt(route)
+       val best = (findBest !? candidates.toList).asInstanceOf[List[Int]]
+       if(tempList == best) {
+         reply(best)
+       } else {
+         func(best)
+       }
+     }
+     loop {
+       react {
+         case route: List[Int] => {
+           func(route)
+         }
+       }
+     }
+   }
+   val better = for (i <- 0 until routes.length) yield samplepoint !? routes(i)
+   println(better) //Vector(List[Int]) length = 4(routes.length)
+   println(getRouteDistance(better(1).asInstanceOf[List[Int]], distanceTable)) //距離を取得
+   println("actors are executed.")
   }
-
-
-
 }
 
-//class MultiPoint() extends Actor {}
-//class HillClimbing() extends Actor {}
+/*各メソッドの挙動チェック - Start*/
+   //println(routes.toList)
+   //println(routes().map(x => twoOpt(x, distanceTable)))
+   ///*訪問順番のシャッフル*/
+   ///*ここで初期探索点を増やす．*/
+   //val newOrder = shuffleRoute(defaultRoute, seed)
+   ///*経路の距離をもとめる*/
+   //val routeDistance = getRouteDistance(defaultRoute, distanceTable)
+   ///*2-optの実行*/
+   //val candidates = twoOpt(defaultRoute)
+   //val div = 4
+   //val hop = candidates.length/div
+   //for(i <- 0 until div) {
+   //  println(candidates.slice(hop*i, hop*i+hop))
+   //}
+   //println(checkTwoOpt)
+/*各メソッドの挙動チェック - End*/
 
 /*都市の巡回する順番をかえる関数*/
 /*citycordiから各都市間の距離を算出する関数*/
